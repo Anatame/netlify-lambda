f58a59f50d5d126a8e637752fdced72f17928724
Anatame <anata.me97@gmail.com>
2022-06-25 15:05:26 +0530

    test

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -1,12 +1,13 @@
 import fetch from "node-fetch";
+const CloudflareBypasser = require('cloudflare-bypasser');
 
 exports.handler = async function (event, context) {
 
     const url = "https://fmovies.to/search?keyword=moon+knight&vrf=%2FmTFtmbuaDGqr4RtKYBwD%2BIV"
 
-    // Make a request for a user with a given ID
-    let response = await fetch(url)
-    let body = await response.text(); 
+    let cf = new CloudflareBypasser();
+    let response = await cf.request(url)
+    let body = response.body // <!DOCTYPE html><html lang="en">...
 
     return {
       statusCode: 200,
