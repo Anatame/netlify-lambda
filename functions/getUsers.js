e4201fb676cf3b2cd1c70576a8e26af8499f2299
Anatame <anata.me97@gmail.com>
2022-06-25 14:01:13 +0530

    test

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -1,15 +1,15 @@
-const axios = require('axios').default;
-const fetch = require("node-fetch")
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 exports.handler = async function (event, context) {
 
-    const url = "https://fmovies.to"
+    const url = "https://fmovies.to/search?keyword=moon+knight&vrf=%2FmTFtmbuaDGqr4RtKYBwD%2BIV"
 
     // Make a request for a user with a given ID
-    let response = fetch(url)
+    let response = await fetch(url)
+    let body = await response.text(); 
 
     return {
       statusCode: 200,
-      body: response.body,
+      body: body,
     };
   }
